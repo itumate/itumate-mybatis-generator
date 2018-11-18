211606533573f62028756c46834732effe965e19
MinGRn <MinGRn97@gmail.com>
2018-11-18 18:47:18 +0800

    新增:Mybatis 生成工具;

---
+++ src/main/java/com/mingrn/mybatis/generator/GenerateMain.java
@@ -0,0 +1,21 @@
+package com.mingrn.mybatis.generator;
+
+public class GenerateMain {
+
+	private static final String JDBC_URL = "jdbc:mysql://localhost/helper-system";
+	private static final String JDBC_USERNAME = "root";
+	private static final String JDBC_PASSWORD = "admin";
+	private static final String AUTHOR = "MinGRn <br > MinGRn97@gmail.com";
+	private static final String PROJECT_PATH = System.getProperty("user.dir");
+	private static final String PROJECT_PACKAGE = "com.mingrn.keeper.gateway";
+
+	public static void main(String[] args) {
+
+		System.out.println(PROJECT_PATH);
+		String[][] tableNames = {
+				{"sys_gateway", "SysGateway", "系统网关", "id", "String"}
+		};
+
+//		CodeUtil.create(tableNames, AUTHOR, JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD, PROJECT_PATH, PROJECT_PACKAGE);
+	}
+}
