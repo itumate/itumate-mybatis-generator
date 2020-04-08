69747f104f538c2c04ab6a95d9dcdb46dc8830b5
mingrn97 <mingrn97@gmail.com>
2020-04-08 17:53:13 +0800

    feat(Eureka 注册中心面板): 样式修改
    1. 修改 Eureka 注册中心面板
    2. 删除废弃文件

--- src/main/java/com/mingrn/itumate/mybatis/generator/GenerateMain.java
+++ src/main/java/com/mingrn/itumate/mybatis/generator/GenerateMain.java
@@ -1,21 +1,23 @@
 package com.mingrn.itumate.mybatis.generator;
 
+import com.mingrn.itumate.mybatis.generator.util.CodeUtil;
+
 public class GenerateMain {
 
-	private static final String JDBC_URL = "jdbc:mysql://localhost/helper-system";
-	private static final String JDBC_USERNAME = "root";
-	private static final String JDBC_PASSWORD = "admin";
-	private static final String AUTHOR = "MinGRn <br > MinGRn97@gmail.com";
-	private static final String PROJECT_PATH = System.getProperty("user.dir");
-	private static final String PROJECT_PACKAGE = "com.mingrn.itumate.gateway";
+	private static final String JDBC_URL = "jdbc:mysql://rm-bp12hk8xofys5ihdm973.mysql.rds.aliyuncs.com:3306/oneroad_uat?useUnicode=true&amp;characterEncoding=utf-8";
+	private static final String JDBC_USERNAME = "yilutong_uat";
+	private static final String JDBC_PASSWORD = "5M4IPc4zwsBSXHCc";
+	private static final String AUTHOR = "zhangshilin <br > zhang.shilin@yilutong.com";
+	private static final String PROJECT_PATH = "C:\\Users\\MinGR\\Downloads\\opera autoupdate";
+	private static final String PROJECT_PACKAGE = "com.yilutong.crm";
 
 	public static void main(String[] args) {
 
 		System.out.println(PROJECT_PATH);
 		String[][] tableNames = {
-				{"sys_gateway", "SysGateway", "系统网关", "id", "String"}
+				{"crm_service_assessment", "CrmServiceAssessment", "服务评价数据表", "object_id", "Long"}
 		};
 
-//		CodeUtil.create(tableNames, AUTHOR, JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD, PROJECT_PATH, PROJECT_PACKAGE);
+		CodeUtil.create(tableNames, AUTHOR, JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD, PROJECT_PATH, PROJECT_PACKAGE);
 	}
 }
